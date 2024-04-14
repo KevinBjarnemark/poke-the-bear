21e27d1d10d9fcae711347842aad11394554824c
KevinBjarnemark <kevin.bjarnemark1@gmail.com>
2024-04-14 16:56:21 +0200

    Added a waiting function that introduces a delay before reseting the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,28 @@ document.addEventListener("DOMContentLoaded", function() {
     runGame();
 });
 
+/**
+ * Waits for a specified number of milliseconds.
+ * This function can be used to introduce a delay in asynchronous operations.
+ *
+ * @param {number} ms The number of milliseconds to wait before resolving the promise.
+ * @returns {Promise<boolean>} A promise that resolves to true after the specified delay.
+ */
+const waitMs = async (ms) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(true)
+        }, ms);
+    });
+};
+
 function runGame () {
     let rageMeter = 0; // 0 - 100
     let filledRageMeterElement = document.getElementById("filled-rage-meter");
     let bearImage = document.getElementById("bear").children[0];
 
-    function handlePoke () {
-        // Update the rage meter and adjust the css width accordingly
-
+    async function handlePoke () {
+        // Change the bear image at specified levels
         switch (true) {
             default: bearImage = "/assets/images/bear/bear_0.png";
             case rageMeter < 50: {
@@ -28,11 +42,13 @@ function runGame () {
             }
         }
 
+        // Update the rage meter and adjust the css width accordingly
         if (rageMeter < 100) {
             rageMeter += Math.random() * 15; // Increment by 0-15
             // Set the css width (limited)
             filledRageMeterElement.style.width = `${Math.min(rageMeter, 100)}%`;
         }else{
+            await waitMs(5000); // Wait 5 seconds before resetting the game
             rageMeter = 0; // Reset to 0
             filledRageMeterElement.style.width = `${rageMeter}%`;
         }
