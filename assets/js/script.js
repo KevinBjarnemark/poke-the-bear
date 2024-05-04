3b690495e3b0189d01eb5b838f7037660c26b475
KevinBjarnemark <kevin.bjarnemark1@gmail.com>
2024-05-04 05:50:58 +0200

    Moved all nested functions to be accessed by the 'global scope'

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,109 +64,120 @@ async function runWelcome (globalHTML, globalVariables) {
     runGameSetup(globalHTML, globalVariables);
 }
 
+function addPlayerToList (globalHTML, globalVariables) {
+    // Create player element
+    let playerElement = document.createElement("div");
+
+    // Set the player name as the innerText
+    let usernameSpan = document.createElement("span");
+    setInnerText(usernameSpan, globalVariables.usernameInput);
+    playerElement.appendChild(usernameSpan);
+
+    // Create a 'remove player button'
+    let removePlayerButton = document.createElement("button");
+    removePlayerButton.innerText = "X";
+    removePlayerButton.className = "remove-player-button"
+    playerElement.appendChild(removePlayerButton);
+    // Listen to the 'remove player click'
+    removePlayerButton.addEventListener("click", function() {
+        /* Remove the playerElement, 
+        note! this will remove the button and stop listening to 
+        button clicks */
+        playerElement.remove(); 
+    });
+
+    // Push player element to container
+    globalHTML.playerList.appendChild(playerElement);
+    // Reset username after submission
+    globalVariables.usernameInput = ""; 
+    globalHTML.usernameInput.value = "";
+    // Reset errors
+    setInnerText(globalHTML.addPlayerError, "");
+    setInnerText(globalHTML.playButtonError, "");
+}
+
+function handlePlayButtonClick (globalHTML, globalVariables) {
+    // Create the players array
+    const playersArray = getChildrenValuesFromSpan(globalHTML.playerList, 
+        "textContent");
+
+    // Run if at least 2 players have been added
+    if (playersArray.length > 1) {
+        // Hide the game set up and show the game area
+        globalHTML.gameSetup.style.display = "none";
+        globalHTML.gameArea.style.display = "block";
+        // Send all players into the game
+        runGame([...playersArray], globalHTML, globalVariables);
+    }else {
+        // Set play button error 
+        setInnerText(globalHTML.playButtonError, "Please add another player");
+        setInnerText(globalHTML.addPlayerError, ""); // Keep UI minimalistic
+    }
+}
+
+function handleAddPlayerButtonClick (globalHTML, globalVariables) {
+    switch(true){
+        // Username length is less than 1 character
+        case globalVariables.usernameInput.length < 1: {
+            setInnerText(globalHTML.addPlayerError, 
+                "Username has to be at least 1 character");
+            setInnerText(globalHTML.playButtonError, "");
+            break;
+        }
+        // Username is too long (24 characters)
+        case globalVariables.usernameInput.length >= 24: {
+            setInnerText(globalHTML.addPlayerError, "Username is too long");
+            setInnerText(globalHTML.playButtonError, "");
+            break;
+        }
+        // There are more than 10 players
+        case globalHTML.playerList.children.length > 150: {
+            setInnerText(globalHTML.addPlayerError, 
+                "The limit for this game is 150 players");
+            setInnerText(globalHTML.playButtonError, "");
+            break;
+        }
+        // Username already exists
+        case getChildrenValuesFromSpan(globalHTML.playerList, "innerText").includes(
+            globalVariables.usernameInput): {
+            setInnerText(globalHTML.addPlayerError, "Username already exists");
+            setInnerText(globalHTML.playButtonError, "");
+            break;
+        }
+        // Run if validation passes
+        default: {
+            addPlayerToList(globalHTML, globalVariables);
+            break;
+        }
+    }
+}
+
+
+function handleChangeUsername (globalVariables, e) {
+    globalVariables.usernameInput = e.target.value;
+}
+
 // Run the game set up
 async function runGameSetup (globalHTML, globalVariables) {
-    function addPlayerToList (globalHTML) {
-        // Create player element
-        let playerElement = document.createElement("div");
-
-        // Set the player name as the innerText
-        let usernameSpan = document.createElement("span");
-        setInnerText(usernameSpan, globalVariables.usernameInput);
-        playerElement.appendChild(usernameSpan);
-
-        // Create a 'remove player button'
-        let removePlayerButton = document.createElement("button");
-        removePlayerButton.innerText = "X";
-        removePlayerButton.className = "remove-player-button"
-        playerElement.appendChild(removePlayerButton);
-        // Listen to the 'remove player click'
-        removePlayerButton.addEventListener("click", function() {
-            /* Remove the playerElement, 
-            note! this will remove the button and stop listening to 
-            button clicks */
-            playerElement.remove(); 
-        });
-
-        // Push player element to container
-        globalHTML.playerList.appendChild(playerElement);
-        // Reset username after submission
-        globalVariables.usernameInput = ""; 
-        globalHTML.usernameInput.value = "";
-        // Reset errors
-        setInnerText(globalHTML.addPlayerError, "");
-        setInnerText(globalHTML.playButtonError, "");
-    }
-
-    function handlePlayButtonClick (globalHTML) {
-        // Create the players array
-        const playersArray = getChildrenValuesFromSpan(globalHTML.playerList, 
-            "textContent");
-
-        // Run if at least 2 players have been added
-        if (playersArray.length > 1) {
-            // Hide the game set up and show the game area
-            globalHTML.gameSetup.style.display = "none";
-            globalHTML.gameArea.style.display = "block";
-            // Send all players into the game
-            runGame([...playersArray], globalHTML, globalVariables);
-        }else {
-            // Set play button error 
-            setInnerText(globalHTML.playButtonError, "Please add another player");
-            setInnerText(globalHTML.addPlayerError, ""); // Keep UI minimalistic
-        }
-    }
-
-    function handleAddPlayerButtonClick (globalHTML, globalVariables) {
-        switch(true){
-            // Username length is less than 1 character
-            case globalVariables.usernameInput.length < 1: {
-                setInnerText(globalHTML.addPlayerError, 
-                    "Username has to be at least 1 character");
-                setInnerText(globalHTML.playButtonError, "");
-                break;
-            }
-            // Username is too long (24 characters)
-            case globalVariables.usernameInput.length >= 24: {
-                setInnerText(globalHTML.addPlayerError, "Username is too long");
-                setInnerText(globalHTML.playButtonError, "");
-                break;
-            }
-            // There are more than 10 players
-            case globalHTML.playerList.children.length > 150: {
-                setInnerText(globalHTML.addPlayerError, 
-                    "The limit for this game is 150 players");
-                setInnerText(globalHTML.playButtonError, "");
-                break;
-            }
-            // Username already exists
-            case getChildrenValuesFromSpan(globalHTML.playerList, "innerText").includes(
-                globalVariables.usernameInput): {
-                setInnerText(globalHTML.addPlayerError, "Username already exists");
-                setInnerText(globalHTML.playButtonError, "");
-                break;
-            }
-            // Run if validation passes
-            default: {
-                addPlayerToList(globalHTML);
-                break;
-            }
-        }
-    }
-
-    function handleChangeUsername (globalVariables, e) {
-        globalVariables.usernameInput = e.target.value;
-    }
-
     // Event listeners [username, play button, add player]
     globalHTML.usernameInput.addEventListener("change", (e) => {
         handleChangeUsername(globalVariables, e)});
 
     globalHTML.playButton.addEventListener("click", () => {
-        handlePlayButtonClick(globalHTML)});
+        handlePlayButtonClick(globalHTML, globalVariables)});
 
     globalHTML.addPlayerButton.addEventListener("click", () => {
         handleAddPlayerButtonClick(globalHTML, globalVariables)});
+}
+
+function setRageMeter (globalHTML, globalVariables, value, increment=false) {
+    if (increment){
+        globalVariables.rageMeter += value;
+    }else{
+        globalVariables.rageMeter = value;
+    }
+    // Set (and limit) the css width accordalHTML.bearImageingly
+    globalHTML.filledRageMeter.style.width = `${Math.min(globalVariables.rageMeter, 100)}%`;
 }
 
 // Run game
@@ -179,21 +190,10 @@ function runGame (playersArray, globalHTML, globalVariables) {
     // Listen to poke button clicks
     globalHTML.pokeButton.addEventListener("click", () => {handlePoke(globalHTML)});
 
-
     // Choose player randomly and set the player hint
     if (globalVariables.chosenPlayer === null){
         globalVariables.chosenPlayer = choseRandomPlayer(globalVariables.alivePlayers);
         setInnerText(globalHTML.playerHint, `${globalVariables.chosenPlayer} it's your turn!`);
-    }
-
-    function setRageMeter (value, increment=false) {
-        if (increment){
-            globalVariables.rageMeter += value;
-        }else{
-            globalVariables.rageMeter = value;
-        }
-        // Set (and limit) the css width accordalHTML.bearImageingly
-        globalHTML.filledRageMeter.style.width = `${Math.min(globalVariables.rageMeter, 100)}%`;
     }
 
     async function handlePoke (globalHTML) {
@@ -201,7 +201,7 @@ function runGame (playersArray, globalHTML, globalVariables) {
         globalHTML.pokeButton.disabled = true; 
 
         /* Increment rage meter by 0-15 */
-        setRageMeter(Math.random() * 15, true);
+        setRageMeter(globalHTML, globalVariables, Math.random() * 15, true);
 
         // Change the bear image at specified levels
         let imageName;
@@ -227,11 +227,11 @@ function runGame (playersArray, globalHTML, globalVariables) {
             if (globalVariables.alivePlayers.length === 1){
                 setInnerText(globalHTML.playerHint, `${globalVariables.alivePlayers[0]}, you won!`);
                 await waitMs(5000); // Wait 5 seconds before resetting the game
-                setRageMeter(0);
+                setRageMeter(globalHTML, globalVariables, 0);
                 resetGame(globalHTML);
             }else{
                 // If there are players, reset the rage meter 
-                setRageMeter(0);
+                setRageMeter(globalHTML, globalVariables, 0);
                 globalVariables.chosenPlayer = choseRandomPlayer(globalVariables.alivePlayers);
                 setInnerText(globalHTML.playerHint, `${globalVariables.chosenPlayer} it's your turn!`);
             }
