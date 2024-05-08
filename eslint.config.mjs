3b72c827fdfab4e9e4c9248d7af28243b9db110c
KevinBjarnemark <kevin.bjarnemark1@gmail.com>
2024-05-08 19:22:19 +0200

    Added (a page-title CSS class, hidden headings) Wrapped main components in sections

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import pluginJs from "@eslint/js";
 export default [
   {languageOptions: { globals: globals.browser },
     rules: {
-      // Set the maximum line length to 80 characters (or any other limit you prefer)
+      // Character limit 
       'max-len': ['error', { code: 80 }],
     },
   },
